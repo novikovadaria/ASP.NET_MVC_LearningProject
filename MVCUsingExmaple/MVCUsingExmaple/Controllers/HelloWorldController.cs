75a6edeed42dd1381dc83770c13bfeed0234dc77
79384 <smb_udk@vk.com>
2023-12-09 02:39:26 +0700

    Cats creating app

---
+++ MVCUsingExmaple/MVCUsingExmaple/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCUsingExmaple.Models;
+
+namespace MVCUsingExmaple.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<CatViewModel> cats = new List<CatViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(cats);
+        }
+
+        public IActionResult Create()
+        {
+            var catModel = new CatViewModel();
+            return View(catModel);
+        }
+
+        public IActionResult CreatCat(CatViewModel catViewModel)
+        {
+            cats.Add(catViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ MVCUsingExmaple/MVCUsingExmaple/Models/CatViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MVCUsingExmaple.Models
+{
+    public class CatViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
